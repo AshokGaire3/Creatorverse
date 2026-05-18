a743e7850db6b5ed2cfc25707aa12e265cbea9be
Ashok_G <117615959+AshIVg@users.noreply.github.com>
2026-05-18 00:04:30 -0400

    feat: configure environment variables for supabase

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // TODO: Replace with your actual Supabase URL and API Key
-const URL = 'insert your Project URL here';
-const API_KEY = 'insert your Project API key here';
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const API_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, API_KEY);
